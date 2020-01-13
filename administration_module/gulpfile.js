853176d326ed7919545ea3890813df495ce4c4d5
Derzhavin <derzhavind@bk.ru>
2020-01-13 22:47:33 +0300

    added gulp

---
+++ administration_module/gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const less = require('gulp-less');
+const babel = require('gulp-babel');
+const babelCore = require('babel-core');
+const pug = require('gulp-pug');
+const del = require('del');
+
+gulp.task('cleanJs', () => del('./public/JSPagesProcessing/*.js'));
+gulp.task('makeJs', () => gulp.src('./JSPagesProcessing/*.js').pipe(babel()).pipe(gulp.dest('./public/JSPagesProcessing/')));
+gulp.task('cleanStyles', () => del('./public/styles/*.css'));
+gulp.task('makeStyles', () => gulp.src('./less/*.less').pipe(less()).pipe(gulp.dest('./public/styles/')));
+gulp.task('default', gulp.parallel((gulp.series('cleanJs', 'makeJs')),(gulp.series('cleanStyles', 'makeStyles'))));
