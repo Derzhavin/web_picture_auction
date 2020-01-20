45dcd864905abc9b965ac891d929b1d4c95162b5
Derzhavin <derzhavind@bk.ru>
2020-01-20 21:47:39 +0300

    participants front

--- administration_module/gulpfile.js
+++ administration_module/gulpfile.js
@@ -5,8 +5,8 @@ const babelCore = require('babel-core');
 const pug = require('gulp-pug');
 const del = require('del');
 
-gulp.task('cleanJs', () => del('./public/JSPagesProcessing/*.js'));
-gulp.task('makeJs', () => gulp.src('./JSPagesProcessing/*.js').pipe(babel()).pipe(gulp.dest('./public/JSPagesProcessing/')));
+gulp.task('cleanJs', () => del('./public/pageProcessing.js'));
+gulp.task('makeJs', () => gulp.src('./pageProcessing.js').pipe(babel()).pipe(gulp.dest('./public/')));
 gulp.task('cleanStyles', () => del('./public/*.css'));
 gulp.task('makeStyles', () => gulp.src('*.less').pipe(less()).pipe(gulp.dest('./public/')));
 gulp.task('default', gulp.parallel((gulp.series('cleanJs', 'makeJs')),(gulp.series('cleanStyles', 'makeStyles'))));
--- administration_module/routes.js
+++ administration_module/routes.js
@@ -5,21 +5,34 @@ var path = require('path');
 var router = express.Router();
 router.use(bodyParser.json());
 
-var arts_json = require('./public/jsons/arts');
-var persons_json = require('./public/jsons/persons');
+var arts = require('./public/jsons/arts');
+var participants = require('./public/jsons/participants');
 var settings = require('./public/jsons/settings');
 
 
 router.get('/main', function(req,res,next) {
-    res.render('main', {title:'Main', arts: arts_json});
+    res.render('main', {title:'Main', arts: arts});
 });
 
 router.get('/settings', function(req,res,next) {
+    if (!settings || Object.keys(settings).length === 0) {
+        let date = new Date();
+
+        let format = number => number / 10 >= 1 ? number : '0' + number;
+
+        settings = {
+            "date": date.getFullYear() + '-' + format(date.getMonth() + 1) + '-' + format(date.getDate()),
+            "time": format(date.getHours())+ ':' + format(date.getMinutes()),
+            "timeout": "05:00",
+            "duration": "00:10",
+            "pause": "00:05"
+        }
+    }
     res.render('settings', {title:'Auction settings', item: settings});
 });
 
 router.get('/participants', function(req,res,next) {
-    res.render('participants', {title:'Participants', persons: persons_json});
+    res.render('participants', {title:'Participants', participants: participants});
 });
 
 module.exports = router;
