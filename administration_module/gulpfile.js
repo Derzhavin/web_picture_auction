889b644bb75a0b0b3cb2f47ef9b5779933af4643
Derzhavin <derzhavind@bk.ru>
2020-01-22 17:39:07 +0300

    front refactoring, added images uploading

--- administration_module/gulpfile.js
+++ administration_module/gulpfile.js
@@ -1,3 +1,4 @@
+
 const gulp = require('gulp');
 const less = require('gulp-less');
 const babel = require('gulp-babel');
@@ -7,6 +8,6 @@ const del = require('del');
 
 gulp.task('cleanJs', () => del('./public/pageProcessing.js'));
 gulp.task('makeJs', () => gulp.src('./pageProcessing.js').pipe(babel()).pipe(gulp.dest('./public/')));
-gulp.task('cleanStyles', () => del('./public/*.css'));
-gulp.task('makeStyles', () => gulp.src('*.less').pipe(less()).pipe(gulp.dest('./public/')));
+gulp.task('cleanStyles', () => del('./public/styles/*.css'));
+gulp.task('makeStyles', () => gulp.src('./styles/*.less').pipe(less()).pipe(gulp.dest('./public/styles/')));
 gulp.task('default', gulp.parallel((gulp.series('cleanJs', 'makeJs')),(gulp.series('cleanStyles', 'makeStyles'))));
