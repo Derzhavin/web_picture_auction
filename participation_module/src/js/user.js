3d5889592eac01b122484828660192a6c1826c21
Derzhavin <derzhavind@bk.ru>
2020-01-26 20:46:57 +0300

    added user purchases gui

--- participation_module/src/js/user.js
+++ participation_module/src/js/user.js
@@ -2,6 +2,7 @@ import '../less/buttons.less';
 
 import {setupHeader} from "./headers";
 import {setupDialogs} from "./dialogs";
+import {} from "./containers"
 
 $(document).ready(() => {
     setupHeader();
